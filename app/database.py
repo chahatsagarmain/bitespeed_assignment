a56e44917e7210510c3cd1a18d3a72ed3ba44faa
chahat sagar <chahatsagar2003@gmail.com>
2026-03-03 18:33:01 +0530

    init db
    Signed-off-by: chahat sagar <chahatsagar2003@gmail.com>

---
+++ app/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+# load environment variables from .env file
+load_dotenv()
+
+# example: postgresql://user:password@localhost/dbname
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost/contacts")
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+Base = declarative_base()
